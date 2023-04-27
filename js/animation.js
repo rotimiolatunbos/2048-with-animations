e8db5673e16ef4b8a5a65d14330471ee4e0b0304
Rotimi <olatunbosun.rotimi@gmail.com>
2023-04-27 06:58:37 +0100

    change to tab to spaces

--- js/animation.js
+++ js/animation.js
@@ -1,114 +1,114 @@
 class ScalingAnimation {
-	constructor(tileIndex, app) {
-		this.tileIndex = tileIndex;
-		this.app = app;
+  constructor(tileIndex, app) {
+    this.tileIndex = tileIndex;
+    this.app = app;
 
-		this.element = document.querySelector(`#t${this.tileIndex}`);
-	}
+    this.element = document.querySelector(`#t${this.tileIndex}`);
+  }
 
-	get animationInfo() {
-		return {
-			targets: this.element,
-			delay: 300,
-			duration: 700,
-			keyframes: [
-				{scale: 1.1},
-				{scale: 1},
-			],
-			easing: 'easeOutElastic',
-			begin: function() {
-				setTimeout(function() {
-					const number = this.app.board[this.tileIndex];
-					
-					this.element.innerText = number;
-					this.element.classList.remove('shown');
-					this.element.classList.forEach(function(cl) {
-						if (cl.startsWith('n')) {
-							this.element.classList.remove(cl);
-						}
-					}.bind(this));
-					if (number) { 
-						this.element.classList.add(`n${number}`, 'shown') 
-					} 
-				}.bind(this), 200)
-					
-			}.bind(this),
-		}
-	}
+  get animationInfo() {
+    return {
+      targets: this.element,
+      delay: 300,
+      duration: 700,
+      keyframes: [
+        {scale: 1.1},
+        {scale: 1},
+      ],
+      easing: 'easeOutElastic',
+      begin: function() {
+        setTimeout(function() {
+          const number = this.app.board[this.tileIndex];
+          
+          this.element.innerText = number;
+          this.element.classList.remove('shown');
+          this.element.classList.forEach(function(cl) {
+            if (cl.startsWith('n')) {
+              this.element.classList.remove(cl);
+            }
+          }.bind(this));
+          if (number) { 
+            this.element.classList.add(`n${number}`, 'shown') 
+          } 
+        }.bind(this), 200)
+          
+      }.bind(this),
+    }
+  }
 }
 
 class SlidingAnimation {
-	constructor(from, to, fromValue, toValue, app) {
-		this.from = from;
-		this.to = to;
-		this.fromValue = fromValue;
-		this.toValue = toValue;
-		this.app = app;
+  constructor(from, to, fromValue, toValue, app) {
+    this.from = from;
+    this.to = to;
+    this.fromValue = fromValue;
+    this.toValue = toValue;
+    this.app = app;
 
-		this.element = document.createElement('div');
-		this.element.innerText = this.fromValue;
-		this.element.classList.add(`t${from}`, `n${this.fromValue}`, 'tile', 'hidden')
-	}
+    this.element = document.createElement('div');
+    this.element.innerText = this.fromValue;
+    this.element.classList.add(`t${from}`, `n${this.fromValue}`, 'tile', 'hidden')
+  }
 
-	static updateTile(i, app) {
-		const tile = document.querySelector(`#t${i}`);
-		const number = app.board[i] ? app.board[i] : '';
-		
-		tile.innerText = number;
-		tile.classList.remove('shown');
-		tile.classList.forEach(function(cl) {
-			if (cl.startsWith('n')) {
-				tile.classList.remove(cl);
-			}
-		});
-		if (number) { 
-			tile.classList.add(`n${number}`, 'shown') 
-		} 
-	}
+  static updateTile(i, app) {
+    const tile = document.querySelector(`#t${i}`);
+    const number = app.board[i] ? app.board[i] : '';
+    
+    tile.innerText = number;
+    tile.classList.remove('shown');
+    tile.classList.forEach(function(cl) {
+      if (cl.startsWith('n')) {
+        tile.classList.remove(cl);
+      }
+    });
+    if (number) { 
+      tile.classList.add(`n${number}`, 'shown') 
+    } 
+  }
 
-	get animationInfo() {
-		const diff = this.from-this.to;
-		const gap = Math.floor(diff/this.app.boardSize);
-		const dist = 20+this.app.tileSize;
-		const animationObj = {
-			targets: this.element,
-			duration: 300,
-			easing: 'easeOutExpo',
-			complete: function() {
-				this.element.remove();
-			}.bind(this)
-		}
+  get animationInfo() {
+    const diff = this.from-this.to;
+    const gap = Math.floor(diff/this.app.boardSize);
+    const dist = 20+this.app.tileSize;
+    const animationObj = {
+      targets: this.element,
+      duration: 300,
+      easing: 'easeOutExpo',
+      complete: function() {
+        this.element.remove();
+      }.bind(this)
+    }
 
-		if (this.toValue > 0 && this.toValue === this.fromValue) {
-			SlidingAnimation.updateTile(this.from, this.app);
-		}
+    if (this.toValue > 0 && this.toValue === this.fromValue) {
+      SlidingAnimation.updateTile(this.from, this.app);
+    }
 
-		if (this.toValue >= 0 && this.fromValue > 0) {
-			SlidingAnimation.updateTile(this.from, this.app);
-			setTimeout(function() {
-				SlidingAnimation.updateTile(this.to, this.app)
-			}.bind(this), 200);
-		}
-		
-		document.querySelector('.board').appendChild(this.element);
+    if (this.toValue >= 0 && this.fromValue > 0) {
+      SlidingAnimation.updateTile(this.from, this.app);
+      setTimeout(function() {
+        SlidingAnimation.updateTile(this.to, this.app)
+      }.bind(this), 200);
+    }
+    
+    document.querySelector('.board').appendChild(this.element);
 
-		if (
-			Math.floor(this.from/this.app.boardSize) !== 
-			Math.floor(this.to/this.app.boardSize)
-		) { // not on the same row; a vertical movement
-			const y = -(gap*dist);
-			
-			return {
-				...animationObj,
-				translateY: y,
-			}
-		} else { // horizontal movement
-			const x = -(diff*dist);
+    if (
+      Math.floor(this.from/this.app.boardSize) !== 
+      Math.floor(this.to/this.app.boardSize)
+    ) { // not on the same row; a vertical movement
+      const y = -(gap*dist);
+      
+      return {
+        ...animationObj,
+        translateY: y,
+      }
+    } else { // horizontal movement
+      const x = -(diff*dist);
 
-			return {
-				...animationObj,
-				translateX: x,
-			}
-		}
-	}
+      return {
+        ...animationObj,
+        translateX: x,
+      }
+    }
+  }
 }
